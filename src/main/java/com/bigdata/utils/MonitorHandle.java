5e40d4979726811437d8b20aefa940d74683c6d7
huangyin0514 <1071324110@qq.com>
2018-07-10 16:55:40 +0800

    部分monitorhandle

--- src/main/java/com/bigdata/utils/MonitorHandle.java
+++ src/main/java/com/bigdata/utils/MonitorHandle.java
@@ -1,5 +1,6 @@
 package com.bigdata.utils;
 
+import com.bigdata.dao.LogMonitorDao;
 import com.bigdata.domian.App;
 import com.bigdata.domian.Message;
 import com.bigdata.domian.Rule;
@@ -8,6 +9,8 @@ import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -27,8 +30,8 @@ public class MonitorHandle implements Serializable {
     private static List<User> userList;
     private static boolean reloaded = false;
 
-    static{
-
+    static {
+        load();
     }
 
     public static Message parser(String line) {
@@ -65,4 +68,100 @@ public class MonitorHandle implements Serializable {
         }
         return false;
     }
+    /**
+     * 加载数据模型，主要是用户列表、应用管理表、组合规则模型、组合用户模型。
+     */
+    public static synchronized void load() {
+        if (userList == null) {
+            userList = loadUserList();
+        }
+        if (appList == null) {
+            appList = loadAppList();
+        }
+        if (ruleMap == null) {
+            ruleMap = loadRuleMap();
+        }
+        if (userMap == null) {
+            userMap = loadUserMap();
+        }
+    }
+
+    /**
+     * 访问数据库获取所有有效的app列表
+     *
+     * @return
+     */
+    private static List<App> loadAppList() {
+        return new LogMonitorDao().getAppList();
+    }
+
+    /**
+     * 访问数据库获取所有有效用户的列表
+     *
+     * @return
+     */
+    private static List<User> loadUserList() {
+        return new LogMonitorDao().getUserList();
+    }
+
+    /**
+     * 封装应用与用户对应的map
+     *
+     * @return
+     */
+    private static Map<String, List<User>> loadUserMap() {
+        //以应用的appId为key，以应用的所有负责人的userList对象为value。
+        //HashMap<String, List<User>>
+        HashMap<String, List<User>> map = new HashMap<String, List<User>>();
+        for (App app : appList) {
+            String userIds = app.getUserId();
+            List<User> userListInApp = map.get(app.getId());
+            //从map中取出的list的没有地址的，为null 所以不能增加元素
+            //遇到这种情况的时候需要给对应的appId的list分配地址空间
+            if (userListInApp == null) {
+                userListInApp = new ArrayList<User>();
+                map.put(app.getId() + "", userListInApp);
+            }
+            String[] userIdArr = userIds.split(",");
+            for (String userId : userIdArr) {
+                userListInApp.add(queryUserById(userId));
+            }
+            map.put(app.getId() + "", userListInApp);
+        }
+        return map;
+    }
+
+    private static User queryUserById(String userId) {
+        for (User user : userList) {
+            if (user.getId() == Integer.parseInt(userId)) {
+                return user;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * 封装应用与规则的map
+     *
+     * @return
+     */
+    private static Map<String,List<Rule>> loadRuleMap() {
+        Map<String, List<Rule>> map = new HashMap<String, List<Rule>>();
+        LogMonitorDao logMonitorDao = new LogMonitorDao();
+        List<Rule> ruleList = logMonitorDao.getRuleList();
+        //将代表rule的list转化成一个map，转化的逻辑是，
+        // 从rule.getAppId作为map的key，然后将rule对象作为value传入map
+        //Map<appId,ruleList>  一个appid的规则信息，保存在一个list中。
+        for (Rule rule : ruleList) {
+            List<Rule> ruleListByAppId = map.get(rule.getAppId() + "");
+            if (ruleListByAppId == null) {
+                ruleListByAppId = new ArrayList<Rule>();
+                map.put(rule.getAppId() + "", ruleListByAppId);
+            }
+            ruleListByAppId.add(rule);
+            map.put(rule.getAppId() + "", ruleList);
+        }
+        return map;
+    }
+
 }
--- src/test/java/SpiltTest.java
+++ src/test/java/SpiltTest.java
@@ -1,4 +1,11 @@
+import com.bigdata.domian.App;
+import com.bigdata.domian.User;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created with IDEA by User1071324110@qq.com
@@ -23,5 +30,51 @@ public class SpiltTest {
     public void subTest() {
         String a = "error: Caused by: java.lang.NoClassDefFoundError: com/starit/gejie/dao/SysNameDao";
         System.out.println(a.substring(4));
+    }
+
+    @Test
+    public void listTest() {
+
+        List<App> appList = new ArrayList<App>();
+        App app1 = new App();
+        app1.setId(1);
+        app1.setUserId("1,3,4");
+        appList.add(app1);
+        App app2 = new App();
+        app2.setId(2);
+        app2.setUserId("1,3,4");
+        appList.add(app2);
+
+
+
+        HashMap<String, List<User>> map = new HashMap<String, List<User>>();
+        for (App app : appList) {
+            String userIds = app.getUserId();
+            List<User> userListInApp = map.get(app.getId());
+           /* if (userListInApp == null) {
+                userListInApp = new ArrayList<User>();
+                map.put(app.getId() + "", userListInApp);
+            }*/
+            String[] userIdArr = userIds.split(",");
+            for (String userId : userIdArr) {
+                User user1 = new User();
+                user1.setId(Integer.parseInt(userId));
+                userListInApp.add(user1);
+            }
+            map.put(app.getId() + "", userListInApp);
+        }
+    }
+
+    @Test
+    public void testListMy() {
+        Map<String, List<String>> map = new HashMap<String, List<String>>();
+        map.put("1", null);
+
+        List<String> strings = map.get("1");
+//        strings = new ArrayList<String>();
+//        map.put("1", strings);
+
+        System.out.println(map);
+
     }
 }
